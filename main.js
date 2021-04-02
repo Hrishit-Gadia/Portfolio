d7a4a9a2d2ca9019feb6fd5d04c4280d9fd41827
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-04-02 12:44:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function changetextbooks(){
+  document.getElementById("txt").innerHTML=(" I usually read when I have a feeling of learning something new and enhance my knowledge. I like to read ' The Adventures Of Tintin ' which is written by hergé. I also read some sporty books as-well-as some riddle kind of books (suduko, guess the word, riddles) I also read some entertaining books.")
+}
+function changetextcode(){
+  document.getElementById("txt").innerHTML=(" I usually code when I have a sprit of creating something new, This webpage is also a example of that. I also like to code when I want to create something special with technology. I have made a lot of creations you can have a look at them at 'hrishitgadia2.whjr.site' Everyday I try to thing of a creation which can help us in future.")
+}
+
+function changetextplay(){
+  document.getElementById("txt").innerHTML=("I usually play outdoor games when the weather is cool and to exercise badminton is one of my favourite one. ")
+}
